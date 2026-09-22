4a758ae75254bac1ca114087a3b1097dd5bdf2fe
Poomer <Poomyakusa1@gmail.com>
2026-09-22 10:03:43 +0700

    3

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,3 @@
+inp: int = int(input('Enter a number less than 25\n'))
+for i in range(inp, 26):
+    print(f"Inside the loop, my variable is {i}")
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,5 @@
+for i in range(11):
+    print(f"Table de {i}: ", end="")
+    for j in range(11):
+        print(i * j, end=" ")
+    print()
